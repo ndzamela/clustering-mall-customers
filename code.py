2af8dfc782c98aff95856f3d24fcadaa1b0edf3f
ndzamela <ndzamela@users.noreply.github.com>
2020-02-13 09:03:10 +0200

    Code file created.

---
+++ code.py
@@ -0,0 +1,43 @@
+# --------------
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.cluster import KMeans
+from sklearn import preprocessing 
+import scipy.cluster.hierarchy as sch
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+
+#Importing the mall dataset with pandas
+
+
+
+# Create an array
+
+
+
+# Using the elbow method to find the optimal number of clusters
+
+
+
+# Plot the graph to visualize the Elbow Method to find the optimal number of cluster  
+
+
+
+# Applying KMeans to the dataset with the optimal number of cluster
+
+
+
+# Visualising the clusters
+
+
+
+# Label encoding and plotting the dendogram
+
+
+
+
